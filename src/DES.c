324a6891bca0b237391d69bb8659d7302cdcd6fa
xiekch <xiekch@qq.com>
2019-10-02 10:22:28 +0800

    update

--- src/DES.c
+++ src/DES.c
@@ -1,5 +1,6 @@
 #include "DES.h"
 #include "stdio.h"
+#include "string.h"
 
 #define KEYBYTES 8
 #define MESSBYTES 8
@@ -7,9 +8,10 @@
 #define MESSLEN 64
 
 typedef struct Key {
-    char k[7];
+    char k[8];
     char c[4];
     char d[4];
+    // char cd[8];
 } Key;
 
 const int IP[] = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
@@ -78,31 +80,60 @@ const int SBox8[] = {13, 2,  8, 4,  6,  15, 11, 1,  10, 9, 3, 14, 5,
 
 const int *SBoxes[] = {SBox1, SBox2, SBox3, SBox4, SBox5, SBox6, SBox7, SBox8};
 
-const int keyE[] = {57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34, 26, 18,
-                    10, 2,  59, 51, 43, 35, 27, 19, 11, 3,  60, 52, 44, 36,
-                    63, 55, 47, 39, 31, 23, 15, 7,  62, 54, 46, 38, 30, 22,
-                    14, 6,  61, 53, 45, 37, 29, 21, 13, 5,  28, 20, 12, 4};
+const int keyPC1[] = {57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34, 26, 18,
+                      10, 2,  59, 51, 43, 35, 27, 19, 11, 3,  60, 52, 44, 36,
+                      63, 55, 47, 39, 31, 23, 15, 7,  62, 54, 46, 38, 30, 22,
+                      14, 6,  61, 53, 45, 37, 29, 21, 13, 5,  28, 20, 12, 4};
 
-const int keyC[] = {14, 17, 11, 24, 1,  5,  3,  28, 15, 6,  21, 10,
-                    23, 19, 12, 4,  26, 8,  16, 7,  27, 20, 13, 2,
-                    41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-                    44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
+const int keyPC2[] = {14, 17, 11, 24, 1,  5,  3,  28, 15, 6,  21, 10,
+                      23, 19, 12, 4,  26, 8,  16, 7,  27, 20, 13, 2,
+                      41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+                      44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
 
-char *permute(char *data, int *permTable, int len) {
-    char *result = (char *)calloc(len / 8 + 1);
+void permute(char *data, int *permTable, int len, char *result) {
     char bit = 0;
     for (int i = 0; i < len; i++) {
         bit = data[(permTable[i] - 1) / 8] & (0x80 >> ((permTable[i] - 1) % 8));
         result[i / 8] |= bit;
     }
-    return result;
 }
 
-Key *subKey(char *key) {
-    Key *keys = (Key *)calloc(16 * sizeof(Key));
-    for (int i = 0; i < 56; i++) {
-        keys[0]
+Key *generateSubKey(char *key) {
+    Key *keys = (Key *)calloc(17 * sizeof(Key));
+    permute(key, keyPC1, 28, keys[0].c);
+    permute(key, keyPC1 + 28, 28, keys[0].d);
+
+    char buffer[10];
+    int shiftBits;
+    for (int i = 1; i <= 16; i++) {
+        if (i == 1 || i == 2 || i == 9 || i == 16) {
+            shiftBits = 1;
+        } else {
+            shiftBits = 2;
+        }
+        int *start = (int *)keys[i].c;
+        char temp = (keys[i].c[0] & (0xff << (8 - shiftBits)));
+        *start <<= shiftBits;
+        keys[i].c[3] ^= temp >> (8 - shiftBits);
+
+        start = (int *)keys[i].d;
+        temp = (keys[i].d[0] & (0xff << (8 - shiftBits)));
+        *start <<= shiftBits;
+        keys[i].d[3] ^= temp >> (8 - shiftBits);
+
+        strncat(buffer, keys[i].c, 28);
+        strncat(buffer + 4, keys[i].d, 28);
+
+        shiftBits = 4;
+        start = (int *)buffer[4];
+        temp = (buffer[4] & (0xff << (8 - shiftBits)));
+        *start <<= shiftBits;
+        buffer[3] ^= temp >> (8 - shiftBits);
+
+        permute(buffer, 48, keyPC2, keys[i].k);
     }
+
+    return keys;
 }
 
 void process(char *data, char *key) {}
@@ -169,4 +200,10 @@ void decrypt(const char *keyFileName, const char *cipherFileName,
     fclose(cipherFile);
 }
 
-void process()
+int main() {
+    char key = {25, 220, 65, 1};
+    Key *keys = generateSubKey(key);
+    for (int i = 0; i < 17; i++) {
+        printf();
+    }
+}
